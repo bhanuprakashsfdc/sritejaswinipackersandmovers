2ccf442d9f7dffb3cf0dbd7ed54dd94547770faf
Bhanu Prakash <bhanusunny7@gmail.com>
2026-03-30 15:43:19 +0530

    fix(map): update map embed URL
    Update the Google Maps iframe source to the new location coordinates for Sri Tejaswini packers and movers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,18 +3,18 @@ import React from 'react';
 const Map: React.FC = () => {
   return (
     <main>
-    <div className="map-container">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14904.330305163488!2d79.40678!3d13.6243132!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4d4ba6bd5a7865%3A0xe747d5a344408872!2sJB%20All%20India%20Packers%20And%20Movers!5e1!3m2!1sen!2sin!4v1718590262869!5m2!1sen!2sin"
-        width="100%"
-        height="450"
-        style={{ border: 0 }}
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        title="Sri Tejaswini packers and movers Location"
-      ></iframe>
-    </div>
+      <div className="map-container">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1201.645544428448!2d79.41493236029346!3d13.614547155513051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e1!3m2!1sen!2sin!4v1774864468072!5m2!1sen!2sin"
+          width="100%"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Sri Tejaswini packers and movers Location"
+        ></iframe>
+      </div>
     </main>
   );
 };
